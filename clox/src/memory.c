8c0a2d9313409772682d948f775b1db9081aafdb
Andy R. Terrel <andy.terrel@gmail.com>
2024-08-07 06:49:22 -0500

    Reformatting with clangd lsp

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -3,41 +3,42 @@
 #include "memory.h"
 #include "vm.h"
 
-void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
-    UNUSED(oldSize);
+void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
+  UNUSED(oldSize);
 
-    if (newSize == 0) {
-        free(pointer);
-        return NULL;
-    }
+  if (newSize == 0) {
+    free(pointer);
+    return NULL;
+  }
 
-    void* result = realloc(pointer, newSize);
-    if (result == NULL) exit(1);
-    return result;
+  void *result = realloc(pointer, newSize);
+  if (result == NULL)
+    exit(1);
+  return result;
 }
 
-static void freeObject(Obj* object) {
-    switch (object->type) {
-        case OBJ_FUNCTION: {
-            ObjFunction* function = (ObjFunction*)object;
-            freeChunk(&function->chunk);
-            FREE(ObjFunction, object);
-            break;
-        }
-        case OBJ_STRING: {
-            ObjString* string = (ObjString*) object;
-            FREE_ARRAY(char, string->chars, string->length + 1);
-            FREE(ObjString, object);
-            break;
-        }
-    }
+static void freeObject(Obj *object) {
+  switch (object->type) {
+  case OBJ_FUNCTION: {
+    ObjFunction *function = (ObjFunction *)object;
+    freeChunk(&function->chunk);
+    FREE(ObjFunction, object);
+    break;
+  }
+  case OBJ_STRING: {
+    ObjString *string = (ObjString *)object;
+    FREE_ARRAY(char, string->chars, string->length + 1);
+    FREE(ObjString, object);
+    break;
+  }
+  }
 }
 
 void freeObjects() {
-    Obj* object = vm.objects;
-    while (object != NULL) {
-        Obj* next = object->next;
-        freeObject(object);
-        object = next;
-    }
+  Obj *object = vm.objects;
+  while (object != NULL) {
+    Obj *next = object->next;
+    freeObject(object);
+    object = next;
+  }
 }
--- clox/src/vm.h
+++ clox/src/vm.h
@@ -9,33 +9,33 @@
 #define STACK_MAX (FRAMES_MAX * UINT8_COUNT)
 
 typedef struct {
-    ObjFunction* function;
-    uint8_t* ip;
-    Value* slots;
+  ObjFunction *function;
+  uint8_t *ip;
+  Value *slots;
 } CallFrame;
 
 typedef struct {
-    CallFrame frames[STACK_MAX];
-    int frameCount;
+  CallFrame frames[STACK_MAX];
+  int frameCount;
 
-    Value stack[STACK_MAX];
-    Value* stackTop;
-    Table globals;
-    Table strings;
-    Obj* objects;
+  Value stack[STACK_MAX];
+  Value *stackTop;
+  Table globals;
+  Table strings;
+  Obj *objects;
 } VM;
 
 typedef enum {
-    INTERPRET_OK,
-    INTERPRET_COMPILE_ERROR,
-    INTERPRET_RUNTIME_ERROR
+  INTERPRET_OK,
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR
 } InterpretResult;
 
 extern VM vm;
 
 void initVM();
 void freeVM();
-InterpretResult interpret(const char* source);
+InterpretResult interpret(const char *source);
 void push(Value value);
 Value pop();
 
