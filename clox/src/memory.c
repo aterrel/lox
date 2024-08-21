7ecd396715010dc7ee515157eacf5b7710a0fc1e
Andy R. Terrel <andy.terrel@gmail.com>
2024-08-21 06:53:01 -0500

    Add garbage collector function and debugging

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -3,8 +3,17 @@
 #include "memory.h"
 #include "vm.h"
 
+#ifdef DEBUG_LOG_GC
+#include "debug.h"
+#include <stdio.h>
+#endif
+
 void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
-  UNUSED(oldSize);
+  if (newSize > oldSize) {
+#ifdef DEBUG_STRESS_GC
+    collectGarbage();
+#endif
+  }
 
   if (newSize == 0) {
     free(pointer);
@@ -18,6 +27,10 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
 }
 
 static void freeObject(Obj *object) {
+#ifdef DEBUG_LOG_GC
+  printf("%p free type %d\n", (void *)object, object->type);
+#endif
+
   switch (object->type) {
   case OBJ_CLOSURE: {
     ObjClosure *closure = (ObjClosure *)object;
@@ -44,6 +57,12 @@ static void freeObject(Obj *object) {
     FREE(ObjUpvalue, object);
     break;
   }
+}
+
+void collectGarbage() {
+#ifdef DEBUG_LOG_GC
+  printf("-- gc begin\n");
+#endif
 }
 
 void freeObjects() {
