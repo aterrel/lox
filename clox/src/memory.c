379fa07711b1c7f00bd7680a47a9c6c9140f4f52
Andy R. Terrel <andy.terrel@gmail.com>
2024-08-22 07:00:03 -0500

    Add graystack

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -37,6 +37,16 @@ void markObject(Obj *object) {
 #endif
 
   object->isMarked = true;
+
+  if (vm.grayCapacity < vm.grayCount + 1) {
+    vm.grayCapacity = GROW_CAPACITY(vm.grayCapacity);
+    vm.grayStack =
+        (Obj **)realloc(vm.grayStack, sizeof(Obj *) * vm.grayCapacity);
+    if (vm.grayStack == NULL)
+      exit(1);
+  }
+
+  vm.grayStack[vm.grayCount++] = object;
 }
 
 void markValue(Value value) {
@@ -84,6 +94,8 @@ void freeObjects() {
     freeObject(object);
     object = next;
   }
+
+  free(vm.grayStack);
 }
 
 static void markRoots() {
