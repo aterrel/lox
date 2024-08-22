0be261d1e839c5ef4ee5958bd9d08648b606ac85
Andy R. Terrel <andy.terrel@gmail.com>
2024-08-22 06:30:34 -0500

    Marking roots

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -26,6 +26,23 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
   return result;
 }
 
+void markObject(Obj *object) {
+  if (object == NULL)
+    return;
+#ifdef DEBUG_LOG_GC
+  printf("%p mark ", (void *)object);
+  printValue(OBJ_VAL(object));
+  printf("\n");
+#endif
+
+  object->isMarked = true;
+}
+
+void markValue(Value value) {
+  if (IS_OBJ(value))
+    markObject(AS_OBJ(value));
+}
+
 static void freeObject(Obj *object) {
 #ifdef DEBUG_LOG_GC
   printf("%p free type %d\n", (void *)object, object->type);
@@ -59,12 +76,6 @@ static void freeObject(Obj *object) {
   }
 }
 
-void collectGarbage() {
-#ifdef DEBUG_LOG_GC
-  printf("-- gc begin\n");
-#endif
-}
-
 void freeObjects() {
   Obj *object = vm.objects;
   while (object != NULL) {
@@ -72,4 +83,24 @@ void freeObjects() {
     freeObject(object);
     object = next;
   }
+}
+
+static void markRoots() {
+  for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+    markValue(*slot);
+  }
+
+  markTable(&vm.globals);
+}
+
+void collectGarbage() {
+#ifdef DEBUG_LOG_GC
+  printf("-- gc begin\n");
+#endif
+
+  markRoots();
+
+#ifdef DEBUG_LOG_GC
+  printf("-- gc end\n");
+#endif
 }
