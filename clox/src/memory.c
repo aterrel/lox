8e4a019109156118168b650f375ea118f33915cf
Andy R. Terrel <andy.terrel@gmail.com>
2024-08-25 10:47:04 -0500

    adding bound methods

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -76,6 +76,12 @@ static void blackenObject(Obj *object) {
 #endif
 
   switch (object->type) {
+  case OBJ_BOUND_METHOD: {
+    ObjBoundMethod *bound = (ObjBoundMethod *)object;
+    markValue(bound->receiver);
+    markObject((Obj *)bound->method);
+    break;
+  }
   case OBJ_CLASS: {
     ObjClass *klass = (ObjClass *)object;
     markObject((Obj *)klass->name);
@@ -118,6 +124,9 @@ static void freeObject(Obj *object) {
 #endif
 
   switch (object->type) {
+  case OBJ_BOUND_METHOD:
+    FREE(OBJ_BOUND_METHOD, object);
+    break;
   case OBJ_CLASS: {
     ObjClass *klass = (ObjClass *)object;
     freeTable(&klass->methods);
